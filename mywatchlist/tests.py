70084f7f1b678bdc0cfff88f33e9095574ea94f2
AlifIsmady <alifismady@gmail.com>
2022-09-22 11:33:11 +0700

    penyelesaian tugas 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -2,12 +2,12 @@ from django.test import TestCase, Client
 from django.urls import reverse
 
 class MyWatchListTest(TestCase):
-        def test(self):
-            html_response = Client().get(reverse('mywatchlist:show_watchlist_html'))
-            xml_response = Client().get(reverse('mywatchlist:show_xml'))
-            json_response = Client().get(reverse('mywatchlist:show_json'))
-            self.assertEqual(html_response.status_code, 200)
-            self.assertEqual(xml_response.status_code, 200)
-            self.assertEqual(json_response.status_code, 200)
+    def test(self):
+        html_response = Client().get(reverse('mywatchlist:show_watchlist_html'))
+        xml_response = Client().get(reverse('mywatchlist:show_xml'))
+        json_response = Client().get(reverse('mywatchlist:show_json'))
+        self.assertEqual(html_response.status_code, 200)
+        self.assertEqual(xml_response.status_code, 200)
+        self.assertEqual(json_response.status_code, 200)
 
 #
