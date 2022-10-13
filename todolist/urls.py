30295e2a95c01e6c8065a0ad2c8516754ff330ac
AlifIsmady <alifismady@gmail.com>
2022-10-13 11:23:34 +0700

    pengerjaan tugas 6

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from todolist.views import show_todolist
-from todolist.views import register, login_user, logout_user, create_task, finished_task, delete_task
+from todolist.views import register, login_user, logout_user, create_task, finished_task, delete_task, show_todolist_json, create_for_modal
 
 
 app_name = 'todolist'
@@ -12,5 +12,7 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('create_task/', create_task, name='create_task'),
     path('finished_task/<int:task_selected>',finished_task,name='finished_task'),
-    path('delete/<int:id>', delete_task, name='delete_task')
+    path('delete/<int:id>', delete_task, name='delete_task'),
+    path('json/', show_todolist_json, name='show_todolist_json'),
+    path('add/', create_for_modal, name='create_for_modal')
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
 from urllib import response
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
@@ -10,6 +11,7 @@ from django.urls import reverse
 from http.client import HTTPResponse
 import datetime
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 from todolist.models import ToDoList
 
@@ -21,6 +23,24 @@ def show_todolist(request):
         'list_item': data_todolist_items,
     }
     return render(request, "todolist.html",context)
+
+@login_required(login_url='/todolist/login/')
+def show_todolist_json(request):
+    data_todolist_items = ToDoList.objects.filter(user=request.user)
+    return HttpResponse(serializers.serialize("json",data_todolist_items), content_type="application/json")
+
+def create_for_modal(request):
+    if request.method == 'POST':
+        new = ToDoList.objects.create(user=request.user, date=datetime.datetime.now(),title=request.POST.get('title'),description=request.POST.get('description'),is_finished=False)
+
+        task = {
+            'title': new.title,
+            'desc' : new.description,
+            'date' : new.date,
+        }
+
+        return JsonResponse(task)
+    
 
 def register(request):
     form = UserCreationForm()
@@ -64,9 +84,9 @@ def finished_task(request,task_selected):
     status = ToDoList.objects.get(id=task_selected)
     
     if status.is_finished == False:
-        status.is_finished == True
+        status.is_finished = True
     else:
-        status.is_finished == False
+        status.is_finished = False
 
     status.save()
     return HttpResponseRedirect(reverse('todolist:show_todolist'))
